553a600de4127b17f7f906c78b1814e8604c7663
Adrian Probierz <probierz.adrian@gmail.com>
2018-10-10 23:38:36 +0200

    MainMenu: add ExitGame

---
+++ MobileGame/Assets/Scripts/UI/Windows/MainWindow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainWindow : MonoBehaviour {
+
+	public void StartGame()
+	{
+
+	}
+	public void ExitGame()
+	{
+#if UNITY_EDITOR
+	UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+	Application.OpenURL(webplayerQuitURL);
+#else
+	Application.Quit();
+#endif
+	}
+}
