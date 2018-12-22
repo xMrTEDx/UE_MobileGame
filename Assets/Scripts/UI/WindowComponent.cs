c220efce7e7e2a1c31611746d72fdd27bc0dd99c
Adrian Probierz <xmrtedx@gmail.com>
2018-12-22 17:31:00 +0100

    added new windows system scripts

---
+++ Assets/Scripts/UI/WindowComponent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowComponent : MonoBehaviour {
+
+	public void ShowWindow()
+	{
+		ClickerGame.Instance.MainCanvasClicker.windowsManager.ShowWindow(this);
+		//Debug.Log("Clicked button: show window");
+	}
+}
