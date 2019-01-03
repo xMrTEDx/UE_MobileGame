7b60984f38ed5bc1aa3a9425fc861f2c1f0de10c
Adrian Probierz <probierz.adrian@gmail.com>
2019-01-03 17:42:48 +0100

    Some prompts added

--- Assets/Scripts/Employees/EmployeesModifier.cs
+++ Assets/Scripts/Employees/EmployeesModifier.cs
@@ -13,12 +13,20 @@ public class EmployeesModifier : MonoBehaviour
 
             // dodac wynagrodzenie dla pracownika kasowane co miesiac
         }
+        else
+        {
+            Prompt.Instance.ShowPrompt("Piekarnie są przepelnione. Nie można zatrudnić nowego pracownika.");
+        }
     }
     public void FireEmployee()
     {
         if (ClickerGame.Instance.EmployeesSystem.FireEmployee())
         {
             //usunac wynagrodzenie pracownika
+        }
+        else
+        {
+             Prompt.Instance.ShowPrompt("Brak pracownikow do zwolnienia.");
         }
     }
     
